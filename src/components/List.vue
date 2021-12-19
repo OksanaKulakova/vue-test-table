cdf5657a2acc413538f59e8b823bf036b3545abc
Оксана Кулакова <oksana5566@yandex.ru>
2021-12-19 16:27:12 +0500

    добавила стили

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,6 +3,11 @@
         <h1>Список</h1>
 
         <table>
+            <tr>
+                <th>Значение 1</th>
+                <th>Значение 2</th>
+                <th>Действия</th>
+            </tr>
             <tr v-for="(user, index) in users" :key="index">
                 <td>
                     <div v-if="user.isActive">
@@ -22,23 +27,23 @@
                 </td>
                 <td>
                     <div v-if="user.isActive">
-                        <button @click="save(index)">Сохранить</button>
+                        <button class="btn btn-save" @click="save(index)">Сохранить</button>
                     </div>
                     <div v-else>
-                        <button @click="edit(index)">Редактировать</button>
+                        <button class="btn" @click="edit(index)">Редактировать</button>
                     </div>  
-                    <button @click="remove(index)">Удалить</button>
+                    <button class="btn btn-remove" @click="remove(index)">Удалить</button>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <input type="text" name="firstName" v-model="newUser.firstName">
+                    <input type="text" name="firstName" placeholder="Введите значение" v-model="newUser.firstName">
                 </td>
                 <td>
-                    <input type="text" name="secondName" v-model="newUser.secondName">
+                    <input type="text" name="secondName" placeholder="Введите значение" v-model="newUser.secondName">
                 </td>
                 <td>
-                    <button @click="add">Добавить</button>
+                    <button class="btn" @click="add">Добавить</button>
                 </td>
             </tr>
             
@@ -109,13 +114,35 @@ export default {
     table {
         margin: auto;
         border-collapse: collapse;
-        border: 1px solid grey;
+        border: 2px solid grey;
     }
     th, td {
         padding: 20px;
         border: 1px solid grey;
+        text-align: left;
+        color: #2c3e50;
+        font-size: 16px;
     }
     input {
+        max-width: 100%;
         color: #2c3e50;
+        font-size: 16px;
+    }
+    .btn {
+        display: block;
+        width: 100%;
+        margin: 5px;
+        padding: 5px;
+        color: white;
+        background-color: #2c3e50;
+        border: 1px solid #2c3e50;
+    }
+    .btn-remove {
+        background-color: #fa0f0f;
+        border: 1px solid #fa0f0f;
+    }
+    .btn-save {
+        background-color: #3705eb;
+        border: 1px solid #3705eb;
     }
 </style>
