8eede09c2f302a3da0e4f04b0011c82aeab4b9e8
Оксана Кулакова <oksana5566@yandex.ru>
2021-12-19 15:49:43 +0500

    реализовала добавление, удаление, редактирование

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,121 @@
 <template>
     <div>
         <h1>Список</h1>
+
+        <table>
+            <tr v-for="(user, index) in users" :key="index">
+                <td>
+                    <div v-if="user.isActive">
+                        <input type="text" name="firstName" v-model="user.firstName">
+                    </div>
+                    <div v-else>
+                        <span>{{ user.firstName }}</span>
+                    </div>   
+                </td>
+                <td>
+                    <div v-if="user.isActive">
+                        <input type="text" name="secondName" v-model="user.secondName">
+                    </div>
+                    <div v-else>
+                        <span>{{ user.secondName }}</span>
+                    </div>  
+                </td>
+                <td>
+                    <div v-if="user.isActive">
+                        <button @click="save(index)">Сохранить</button>
+                    </div>
+                    <div v-else>
+                        <button @click="edit(index)">Редактировать</button>
+                    </div>  
+                    <button @click="remove(index)">Удалить</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" name="firstName" v-model="newUser.firstName">
+                </td>
+                <td>
+                    <input type="text" name="secondName" v-model="newUser.secondName">
+                </td>
+                <td>
+                    <button @click="add">Добавить</button>
+                </td>
+            </tr>
+            
+        </table>
+
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  name: 'List',
+    name: 'List',
+
+    data() {
+        return {
+            newUser: {
+                firstName: null,
+                secondName: null
+            },
+        };
+    },
+
+    computed: {
+        ...mapGetters({
+            users: 'getUsers'
+        }),
+    },
+
+    mounted() {
+        this.$store.dispatch('getUsers');
+    },
+
+    methods: {
+        clear() {
+           this.newUser.firstName = null;
+           this.newUser.secondName = null;
+        },
+
+        add: function() {
+            this.$store.dispatch('addUser', this.newUser)
+            .then(() => {
+                this.clear();
+            })
+        },
+
+        edit: function (index) {
+            this.users[index].isActive = true;
+        },
+
+        save: function(index) {
+            let user = this.users[index];
+            this.$store.dispatch('updateUser', user)
+            .then(() => {
+                this.users[index].isActive = false;
+            })
+        },
+
+        remove: function(index) {
+            let user = this.users[index];
+            this.$store.dispatch('removeUser', user);
+        },
+    }
 }
 </script>
+
+<style scoped>
+    table {
+        margin: auto;
+        border-collapse: collapse;
+        border: 1px solid grey;
+    }
+    th, td {
+        padding: 20px;
+        border: 1px solid grey;
+    }
+    input {
+        color: #2c3e50;
+    }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,53 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    users: null
   },
+
+  getters: {
+    getUsers: state => {
+      return state.users;
+    },
+  },
+
   mutations: {
+    setUsers: (state, payload) => {
+      state.users = payload;
+    },
+    addUser: (state, payload) => {
+      state.users.push(payload);
+    },
+    removeUser: (state, payload) => {
+      let index = state.users.indexOf(payload);
+      state.users.splice(index, 1);
+    },
   },
+
   actions: {
+    getUsers: async (context) => {
+      let {data} = await axios.get('http://localhost:3123/');
+      context.commit('setUsers', data);
+    },
+
+    addUser: async (context, payload) => {
+      let {data} = await axios.post('http://localhost:3123/', payload);
+      context.commit('addUser', data);
+    },
+
+    updateUser: async (context, payload) => {
+      let uuid = payload.uuid;
+      axios.put(`http://localhost:3123/${uuid}`, payload);
+    },
+
+    removeUser: async (context, payload) => {
+      let uuid = payload.uuid;
+      axios.delete(`http://localhost:3123/${uuid}`, payload)      
+      .then(() => {
+        context.commit('removeUser', payload);
+      });
+    },
   },
-  modules: {
-  }
+
 })
